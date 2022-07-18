3824b857cf5da295eaf5a55acca8d9148a80b61a
Danyel J <danyjoeyz@gmail.com>
2022-07-18 00:06:46 -0500

    Se modifican los campos de restaurant

--- restaurant/core/views.py
+++ restaurant/core/views.py
@@ -13,47 +13,48 @@ from .models import Restaurant
 class RestaurantView(View):
 
     @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs): #Modifica el comportamiento y salte restriccion
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, id=0):
         if(id > 0):
             restaurants = list(Restaurant.objects.filter(id=id).values())
-            if len(restaurants) > 0:
-                company = restaurants[0]
-                datos = {'message': "Success", "companies": company}
+            if len(restaurants) > 0: #Si la longitud de restaurant
+                restaurant = restaurants[0]
+                datos = {'message': "Success", "restaurant": restaurant}
             else:
-                datos = {'message': "Companies not found.."}
-            return  JsonResponse(datos)
+                datos = {'message': "Not found.."}
+            return JsonResponse(datos)
         else:
-            companies = list(Restaurant.objects.values())
-            if len(companies) > 0:
-                datos = {'message': "Succes", 'companies': companies}
+            restaurants = list(Restaurant.objects.values()) #Serializamos json
+            if len(restaurants) > 0:
+                datos = {'message': "Succes", 'restaurants': restaurants}
             else:
                 datos = {'message': "Companies not found.."}
             return JsonResponse(datos)
 
     def post(self, request):
-        #print(request.body)
-        jd = json.loads(request.body)
-        Restaurant.objects.create(name=jd['name'], type_restaurant=jd['type_restaurant'], address=jd['address'], phone=jd['phone'] )
+        jdata = json.loads(request.body)
+        Restaurant.objects.create(name=jdata['name'],
+                                  type_restaurant=jdata['type_restaurant'],
+                                  address=jdata['address'], phone=jdata['phone'] )
         datos = {'message': "Success"}
         return JsonResponse(datos)
 
     def put(self, request, id):
-        jd = json.loads(request.body)
+        jdata = json.loads(request.body)
         restaurants = list(Restaurant.objects.filter(id=id).values())
         if len(restaurants) > 0:
             restaurant = Restaurant.objects.get(id=id)
-            restaurant.name = jd['name']
-            restaurant.type_restaurant = jd['type_restaurant']
-            restaurant.address = jd['address']
-            restaurant.phone = jd['phone']
+            restaurant.name = jdata['name']
+            restaurant.type_restaurant = jdata['type_restaurant']
+            restaurant.address = jdata['address']
+            restaurant.phone = jdata['phone']
             restaurant.save()
             datos = {'message': "Succes"}
         else:
-            datos = {'message': "Companies not found.."}
-        return  JsonResponse(datos)
+            datos = {'message': "Not found.."}
+        return JsonResponse(datos)
 
     def delete(self, request, id):
         restaurants = list(Restaurant.objects.filter(id=id).values())
